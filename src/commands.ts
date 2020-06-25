7a8ebb5e3bdbae3b8f5c8cbacb882900bdb135e1
billytheme <30644690+billytheme@users.noreply.github.com>
2020-06-25 13:01:36 +1000

    Fixed and commented help

--- src/commands.ts
+++ src/commands.ts
@@ -6,10 +6,13 @@ import * as help from "./help/help"
 client.on('message', function (message: discord.Message) {
     let lowerMessage = message.content.toLowerCase();
 
+    // If the message contains the command prefix and came from the correct channel, process it
     if (lowerMessage.slice(0, 2) == 'b!' && message.channel.id === process.env.BOT_CHANNEL) {
+        // Only look at the part after the prefix
         switch (lowerMessage.slice(2)) {
             case 'help':
                 help.generateHelpPage()
+                break;
             case 'scanners':
                 scannerRanking.generateRanking();
                 break;
