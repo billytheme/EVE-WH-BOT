73e1bf2aafe8d04a57247744c7d8f96ac61c08d8
billytheme <30644690+billytheme@users.noreply.github.com>
2020-07-03 15:26:51 +1000

    Added tests for killerRanking

--- src/killerRanking/killerRanking.ts
+++ src/killerRanking/killerRanking.ts
@@ -11,11 +11,13 @@ export async function parseKill(event: { data: any; type: string; }) {
 
     if (isfriendlyAttackers(killData)) {
         for (const attackerIndex in killData.attackers) {
-            if (killData.attackers[attackerIndex].alliance_id.toString() === process.env.FRIENDLY_ALLIANCE) {
-                if (killerDictionary[killData.attackers[attackerIndex].character_id] === undefined) {
-                    killerDictionary[killData.attackers[attackerIndex].character_id] = 1;
-                } else {
-                    killerDictionary[killData.attackers[attackerIndex].character_id] += 1;
+            if (killData.attackers[attackerIndex].alliance_id !== undefined) {
+                if (killData.attackers[attackerIndex].alliance_id.toString() === process.env.FRIENDLY_ALLIANCE) {
+                    if (killerDictionary[killData.attackers[attackerIndex].character_id] === undefined) {
+                        killerDictionary[killData.attackers[attackerIndex].character_id] = 1;
+                    } else {
+                        killerDictionary[killData.attackers[attackerIndex].character_id] += 1;
+                    }
                 }
             }
         }
